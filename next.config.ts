4128a1d423aa4148e5183c51e6664d2704868d59
santosh malana <150065221+SantoshMalana@users.noreply.github.com>
2026-07-04 14:30:29 +0530

    fix: replace deprecated Sentry config options (hideSourceMaps, disableLogger)

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,7 @@ export default withSentryConfig(nextConfig, {
   org: process.env.SENTRY_ORG,
   project: process.env.SENTRY_PROJECT,
   widenClientFileUpload: true,
-  hideSourceMaps: true,
-  disableLogger: true,
+  sourcemaps: {
+    deleteSourcemapsAfterUpload: true,
+  },
 })
